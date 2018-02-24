e7ff0c5873d941ba35ad23b36e9b5d86eb056211
ck66 <30546701+ck66@users.noreply.github.com>
2018-02-24 20:08:24 +0800

    Update C72-2.sh

--- C72-2.sh
+++ C72-2.sh
@@ -5,7 +5,7 @@ export PATH
 
 [ "$EUID" -ne '0' ] && echo "Error,This script must be run as root! " && exit 1
 KernelList="$(rpm -qa |grep 'kernel' |awk '{print $1}')"
-[ -z "$(echo $KernelList |grep -o kernel-ml-4.11.8-1.el7.elrepo.x86_64)" ] && echo "Install error." && exit 1
+[ -z "$(echo $KernelList |grep -o kernel-ml-4.10.9-1.el7.elrepo.x86_64)" ] && echo "Install error." && exit 1
 for KernelTMP in `echo "$KernelList"`
  do
   [ "$KernelTMP" != "kernel-ml-4.10.9-1.el7.elrepo.x86_64" ] && echo -ne "Uninstall Old Kernel\n\t$KernelTMP\n" && yum remove "$KernelTMP" -y >/dev/null 2>&1
