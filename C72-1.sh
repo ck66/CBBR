a7e638b8bb01fa0c521c6761dcf9a7077522f1bf
ck66 <30546701+ck66@users.noreply.github.com>
2018-03-21 19:16:42 +0800

    Update C72-1.sh

--- C72-1.sh
+++ C72-1.sh
@@ -5,18 +5,18 @@ export PATH
 
 [ "$EUID" -ne '0' ] && echo "Error,This script must be run as root! " && exit 1
 KernelList="$(rpm -qa |grep 'kernel' |awk '{print $1}')"
-[ -z "$(echo $KernelList |grep -o kernel-ml-4.10.9-1.el7.elrepo.x86_64)" ] && echo "Install error." && exit 1
+[ -z "$(echo $KernelList |grep -o kernel-ml-4.10.1-1.el7.elrepo.x86_64)" ] && echo "Install error." && exit 1
 for KernelTMP in `echo "$KernelList"`
  do
-  [ "$KernelTMP" != "kernel-ml-4.10.9-1.el7.elrepo.x86_64" ] && echo -ne "Uninstall Old Kernel\n\t$KernelTMP\n" && yum remove "$KernelTMP" -y >/dev/null 2>&1
+  [ "$KernelTMP" != "kernel-ml-4.10.1-1.el7.elrepo.x86_64" ] && echo -ne "Uninstall Old Kernel\n\t$KernelTMP\n" && yum remove "$KernelTMP" -y >/dev/null 2>&1
 done
 
 yum remove kernel-headers -y
 # http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/
-yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml-headers-4.10.9-1.el7.elrepo.x86_64.rpm
-yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml-devel-4.10.9-1.el7.elrepo.x86_64.rpm
-yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml-tools-libs-4.10.9-1.el7.elrepo.x86_64.rpm
-yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml-tools-4.10.9-1.el7.elrepo.x86_64.rpm
+yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml-headers-4.10.1-1.el7.elrepo.x86_64.rpm
+yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml-devel-4.10.1-1.el7.elrepo.x86_64.rpm
+yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml-tools-libs-4.10.1-1.el7.elrepo.x86_64.rpm
+yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml-tools-4.10.1-1.el7.elrepo.x86_64.rpm
 
 yum install make gcc -y
 wget -O ./tcp_bbr_powered.c https://gist.github.com/anonymous/ba338038e799eafbba173215153a7f3a/raw/55ff1e45c97b46f12261e07ca07633a9922ad55d/tcp_tsunami.c
